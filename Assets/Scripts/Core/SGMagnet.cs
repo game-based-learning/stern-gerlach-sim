5ac215eb03b4f424b2b28f5c4f146af4e3582359
Aaron Post <aarontpost@gmail.com>
2023-08-08 01:00:25 -0500

    Fixed successive magnet bug

--- Assets/Scripts/Core/SGMagnet.cs
+++ Assets/Scripts/Core/SGMagnet.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SGMagnet : Node
 {
@@ -14,7 +15,15 @@ public class SGMagnet : Node
     public override Vector3 GetStartLocation { get => entrance.transform.position; set => entranceLoc = value; }
     public override Vector3 GetEndLocation { get => exit.transform.position; set => exitLoc = value; }
     public override int GetRotation(GameObject gameObject) {
-        return Mathf.FloorToInt(transform.eulerAngles.x);
+        // deal with absurdly strange and specific edge case
+        print("Euler: " + transform.eulerAngles);
+        if (transform.eulerAngles.y == -180 || transform.eulerAngles.y == 180) {
+            return Mathf.FloorToInt(transform.eulerAngles.y);
+        }
+        else
+        {
+            return Mathf.FloorToInt(Mathf.Repeat(transform.localEulerAngles.x + 180, 360) - 180);
+        }
     } 
     public SGMagnet() {
 
